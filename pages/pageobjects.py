b5fd0c8523781158ffddb7a73995751b5dda203e
jujga <jujgabk@gmail.com>
2022-02-20 21:55:39 +0200

    привел к pep8

--- pages/pageobjects.py
+++ pages/pageobjects.py
@@ -1,8 +1,8 @@
-# не смог одолеть проблему циклического импорта, потому все пейжобжекты поместил в один файл
+# не смог одолеть проблему циклического импорта,
+# потому все пейжобжекты поместил в один файл
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from test_data.credentials import Credentials
 from selenium.webdriver.support import expected_conditions as EC
 
 
@@ -13,43 +13,50 @@ class BasePage(object):
         self.driver.implicitly_wait(5)
         self.wait = WebDriverWait(driver, 10)
 
-
     def browser_back_button_click(self):
         self.driver.back()
 
     @staticmethod
     def goods_heart_button(web_item):
-        return web_item.find_element(By.XPATH, './/span[@data-qaid="add_favorite"]')
+        return web_item.find_element(By.XPATH,
+                                     './/span[@data-qaid="add_favorite"]')
 
     @property
     def comein_link(self):
-        return self.driver.find_element(By.XPATH, '//button[@data-qaid="sign-in"]')
+        return self.driver.find_element(By.XPATH,
+                                        '//button[@data-qaid="sign-in"]')
 
     @property
     def email_button(self):
-        return self.wait.until(EC.element_to_be_clickable((By.XPATH, '//div[@data-qaid="email_btn"]')))
+        return self.wait.until(EC.element_to_be_clickable(
+            (By.XPATH, '//div[@data-qaid="email_btn"]')))
 
     @property
     def email_field(self):
-        return self.driver.find_element(By.XPATH, '//input[@id="email_field"]')
+        return self.driver.find_element(By.XPATH,
+                                        '//input[@id="email_field"]')
 
     @property
     def next_button(self):
-        return self.driver.find_element(By.XPATH, '//button[@id="emailConfirmButton"]')
+        return self.driver.find_element(By.XPATH,
+                                        '//button[@id="emailConfirmButton"]')
 
     @property
     def password_field(self):
-        return self.driver.find_element(By.XPATH, '//input[@id="enterPassword"]')
+        return self.driver.find_element(By.XPATH,
+                                        '//input[@id="enterPassword"]')
 
     @property
     def comein_button(self):
-        return self.driver.find_element(By.XPATH, '//button[@id="enterPasswordConfirmButton"]')
+        return self.driver.find_element(
+            By.XPATH, '//button[@id="enterPasswordConfirmButton"]')
 
     def comein_button_click(self):
         self.comein_button.click()
 
     def go_logined_page(self):
         return LoginedPage(self.driver)
+
 
 class MainPage(BasePage):
     # основная страница до логина
@@ -59,9 +66,8 @@ class MainPage(BasePage):
     @property
     def goods_list(self):
         # эксплисит ждет, пока не появится список товаров
-        return self.wait.until(EC.visibility_of_any_elements_located((By.XPATH, '//div[@data-qaid="product_block"]')))
-
-
+        return self.wait.until(EC.visibility_of_any_elements_located(
+            (By.XPATH, '//div[@data-qaid="product_block"]')))
 
     # def goods_click(self,i):
     #     self.goods_list[i].click()
@@ -72,46 +78,38 @@ class LoginedPage(MainPage):
     def __init__(self, driver):
         super().__init__(driver)
 
-
     @property
     def fav_page_button(self):
-        return self.driver.find_element(By.XPATH, '//button[@data-qaid = "favorite_cabinet_button"]')
+        return self.driver.find_element(
+            By.XPATH, '//button[@data-qaid = "favorite_cabinet_button"]')
 
     def fav_page_button_click(self):
         self.fav_page_button.click()
         return FavoritePage(self.driver)
 
-
-
-    def goods_click(self,i):
+    def goods_click(self, i):
         self.goods_list[i].click()
         return GoodsDetail(self.driver)
 
     @staticmethod
     def good_name_text(web_item):
-        return web_item.find_element(By.XPATH, './/span[@data-qaid="product_name"]').text
+        return web_item.find_element(
+            By.XPATH, './/span[@data-qaid="product_name"]').text
 
     @staticmethod
     def goods_heart_button(web_item):
-        return web_item.find_element(By.XPATH, './/span[@data-qaid="add_favorite"]')
-
-
+        return web_item.find_element(By.XPATH,
+                                     './/span[@data-qaid="add_favorite"]')
 
     @property
     def fav_button_counter_text(self):
-        return self.driver.find_element(By.XPATH, '//div[@data-qaid="counter"]').text
+        return self.driver.find_element(By.XPATH,
+                                        '//div[@data-qaid="counter"]').text
 
     @staticmethod
     def goods_link(web_item):
-        return self.driver.find_element(By.XPATH, '//div[@data-qaid="counter"]').text
-
-
-    # def good_name(self, i):
-    #     return self.goods_list[i].find_element(By.XPATH, './/span[@data-qaid="product_name"]').text
-
-    # def list_goods_heart_button(self, i):
-    #     return self.goods_list[i].find_element(By.XPATH, '//span[@data-qaid="add_favorite"]')
-
+        return self.driver.find_element(By.XPATH,
+                                        '//div[@data-qaid="counter"]').text
 
 
 class FavoritePage(BasePage):
@@ -120,18 +118,18 @@ class FavoritePage(BasePage):
 
     @property
     def fav_button_counter_text(self):
-        return self.driver.find_element(By.XPATH, '//div[@data-qaid="counter"]//span').text
+        return self.driver.find_element(
+            By.XPATH, '//div[@data-qaid="counter"]//span').text
 
     @property
     def del_favs_button(self):
-        return self.driver.find_elements(By.XPATH, '//span[@data-qaid="delete_icon"]')
-      # return self.wait.until(EC.visibility_of_any_elements_located((By.XPATH, '//div[@data-qaid="product_block"]')))
-        # return self.wait.until(EC.visibility_of_any_elements_located((By.XPATH, '//span[@data-qaid="delete_icon"]')))
+        return self.driver.find_elements(By.XPATH,
+                                         '//span[@data-qaid="delete_icon"]')
 
     @property
     def fav_list(self):
         return self.driver.find_elements(By.XPATH,
-                                         '//a[@data-qaid="product_name"]')  # //div[@data-qaid="product_info"]')
+                                         '//a[@data-qaid="product_name"]')
 
 
 class GoodsDetail(BasePage):
@@ -141,14 +139,14 @@ class GoodsDetail(BasePage):
     @property
     def fav_add_button(self):
         return self.driver.find_element(By.XPATH,
-                                         '//span[@data-qaid="add_favorite"]')
+                                        '//span[@data-qaid="add_favorite"]')
 
     @property
     def good_code_text(self):
-        return self.driver.find_element(By.XPATH,
-                                         '//span[@data-qaid="product-sku"]').text
+        return self.driver.find_element(
+            By.XPATH, '//span[@data-qaid="product-sku"]').text
 
     @property
-    def good_name_text(self):
-        return self.driver.find_element(By.XPATH,
-                                        '//h1[@data-qaid="product_name"]').text
+    def good_name_txt(self):
+        return self.driver.find_element(
+            By.XPATH, '//h1[@data-qaid="product_name"]').text
