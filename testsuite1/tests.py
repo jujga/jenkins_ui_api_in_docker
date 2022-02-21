e0401300d1fb148d9a4ad2b5a4472d7a0a47df51
jujga <jujgabk@gmail.com>
2022-02-21 12:19:53 +0200

    реализовал тест def test3_add2fav_1_2_1

--- testsuite1/tests.py
+++ testsuite1/tests.py
@@ -9,10 +9,9 @@ import common
 
 @pytest.mark.parametrize('fav_numbers, add_fav_from_detail', (
         ((0, 1), 'add_from_goods_list'),
-        ((0, 2, 5), 'add_from_goods_list'),
+        ((0, 2, 4), 'add_from_goods_list'),
         ((2, 5), 'add_from_goods_detail')))
-def test1_add2fav(driver, logined_page,
-                  fav_numbers: tuple, add_fav_from_detail: str):
+def test1_add2fav(logined_page, fav_numbers: tuple, add_fav_from_detail: str):
     goods_for_fav = logined_page.goods_list
     fav_good_names_expected = []
     for goods_index in fav_numbers:
@@ -47,16 +46,38 @@ def test1_add2fav(driver, logined_page,
            'который добавлялся в избранное'
 
 
-# добавляем один товар в избранное без предварительного логина
+# добавляется один товар в избранное без предварительного логина
 def test2_add2fav_out_of_login(main_page):
     # запоминаем товар, что добавляется в избранное
     fav_goods_names_expected = \
         {(LoginedPage.good_name_text(main_page.goods_list[1]))}
-    MainPage.goods_heart_button(main_page.goods_list[1]).click()
+    LoginedPage.goods_heart_button(
+        main_page.goods_list[1]).click()
     common.login_steps(main_page)
     logined_page = main_page.go_logined_page()
     check.equal(logined_page.fav_button_counter_text, '1',
                 'Количество избранных на кнопке Избранное')
+    favorite_page = logined_page.fav_page_button_click()
+    check.equal(favorite_page.fav_button_counter_text, '1',
+                'Количество на странице Избранное')
+    assert {i.text for i in favorite_page.fav_list} == \
+           {i for i in fav_goods_names_expected}, \
+           'Названия товаров в избранном'
+
+
+# добавляется с индексом 1 и 2, с индексом 1 убирается
+def test3_add2fav_1_2_1(logined_page):
+    goods_for_fav = logined_page.goods_list
+    LoginedPage.goods_heart_button(
+        goods_for_fav[1]).click()
+    from time import sleep
+    fav_goods_names_expected = {
+        LoginedPage.good_name_text(goods_for_fav[2])}
+    LoginedPage.goods_heart_button(
+        goods_for_fav[2]).click()
+    LoginedPage.goods_heart_button(
+        goods_for_fav[1]).click()
+
     favorite_page = logined_page.fav_page_button_click()
     check.equal(favorite_page.fav_button_counter_text, '1',
                 'Количество на странице Избранное')
